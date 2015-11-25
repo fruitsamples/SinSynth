e6e56bbc949a2039b612b8cb87b31d64cdf2331e
Steve Moser <contact@stevemoser.org>
2015-11-25 15:57:16 -0500

    Add OSX SDK 10.6 (402.20) Docset changes

---
+++ SinSynth.cpp
@@ -0,0 +1,260 @@
+/*	Copyright © 2007 Apple Inc. All Rights Reserved.
+	
+	Disclaimer: IMPORTANT:  This Apple software is supplied to you by 
+			Apple Inc. ("Apple") in consideration of your agreement to the
+			following terms, and your use, installation, modification or
+			redistribution of this Apple software constitutes acceptance of these
+			terms.  If you do not agree with these terms, please do not use,
+			install, modify or redistribute this Apple software.
+			
+			In consideration of your agreement to abide by the following terms, and
+			subject to these terms, Apple grants you a personal, non-exclusive
+			license, under Apple's copyrights in this original Apple software (the
+			"Apple Software"), to use, reproduce, modify and redistribute the Apple
+			Software, with or without modifications, in source and/or binary forms;
+			provided that if you redistribute the Apple Software in its entirety and
+			without modifications, you must retain this notice and the following
+			text and disclaimers in all such redistributions of the Apple Software. 
+			Neither the name, trademarks, service marks or logos of Apple Inc. 
+			may be used to endorse or promote products derived from the Apple
+			Software without specific prior written permission from Apple.  Except
+			as expressly stated in this notice, no other rights or licenses, express
+			or implied, are granted by Apple herein, including but not limited to
+			any patent rights that may be infringed by your derivative works or by
+			other works in which the Apple Software may be incorporated.
+			
+			The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+			MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+			THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+			FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+			OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+			
+			IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+			OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+			SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+			INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+			MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+			AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+			STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+			POSSIBILITY OF SUCH DAMAGE.
+*/
+/*
+	This is a test implementation of a sin wave synth using AUInstrumentBase classes
+	
+	It illustrates a basic usage of these classes
+	
+	It artificially limits the number of notes at one time to 12, so the note-stealing 
+	algorithm is used - you should know how this works!
+	
+	Most of the work you need to do is defining a Note class (see TestNote). AUInstrument manages the
+	creation and destruction of notes, the various stages of a note's lifetime.
+
+	Alot of printfs have been left in (but are if'def out)
+	These can be useful as you figure out how this all fits together. This is true in the AUInstrumentBase
+	classes as well; simply define DEBUG_PRINT to 1 and this turns all this on
+	
+	The project also defines CA_AUTO_MIDI_MAP (OTHER_C_FLAGS). This adds all the code that is needed
+	to map MIDI messages to specific parameter changes. This can be seen in AU Lab's MIDI Editor window
+	CA_AUTO_MIDI_MAP is implemented in AUMIDIBase.cpp/.h
+*/
+
+#include "SinSynth.h"
+ 
+static const UInt32 kMaxActiveNotes = 8;
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+const double twopi = 2.0 * 3.14159265358979;
+
+inline double pow5(double x) { double x2 = x*x; return x2*x2*x; }
+
+#pragma mark SinSynth Methods
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+COMPONENT_ENTRY(SinSynth)
+
+static const AudioUnitParameterID kGlobalVolumeParam = 0;
+static const CFStringRef kGlobalVolumeName = CFSTR("global volume");
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	SinSynth::SinSynth
+//
+// This synth has No inputs, One output
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SinSynth::SinSynth(ComponentInstance inComponentInstance)
+	: AUMonotimbralInstrumentBase(inComponentInstance, 0, 1)
+{
+	CreateElements();
+	
+	Globals()->UseIndexedParameters(1); // we're only defining one param
+	Globals()->SetParameter (kGlobalVolumeParam, 1.0);
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	SinSynth::~SinSynth
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SinSynth::~SinSynth()
+{
+}
+
+
+void SinSynth::Cleanup()
+{
+#if DEBUG_PRINT
+	printf("SinSynth::Cleanup\n");
+#endif
+}
+
+OSStatus SinSynth::Initialize()
+{	
+#if DEBUG_PRINT
+	printf("->SinSynth::Initialize\n");
+#endif
+	AUMonotimbralInstrumentBase::Initialize();
+	
+	SetNotes(kNumNotes, kMaxActiveNotes, mTestNotes, sizeof(TestNote));
+#if DEBUG_PRINT
+	printf("<-SinSynth::Initialize\n");
+#endif
+	
+	return noErr;
+}
+
+OSStatus			SinSynth::GetParameterInfo(		AudioUnitScope					inScope,
+														AudioUnitParameterID			inParameterID,
+														AudioUnitParameterInfo &		outParameterInfo)
+{
+	if (inParameterID != kGlobalVolumeParam) return kAudioUnitErr_InvalidParameter;
+	if (inScope != kAudioUnitScope_Global) return kAudioUnitErr_InvalidScope;
+
+	outParameterInfo.flags = SetAudioUnitParameterDisplayType (0, kAudioUnitParameterFlag_DisplaySquareRoot);
+    outParameterInfo.flags += kAudioUnitParameterFlag_IsWritable;
+	outParameterInfo.flags += kAudioUnitParameterFlag_IsReadable;
+
+	AUBase::FillInParameterName (outParameterInfo, kGlobalVolumeName, false);
+	outParameterInfo.unit = kAudioUnitParameterUnit_LinearGain;
+	outParameterInfo.minValue = 0;
+	outParameterInfo.maxValue = 1.0;
+	outParameterInfo.defaultValue = 1.0;
+	return noErr;
+}
+
+
+#pragma mark TestNote Methods
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+void			TestNote::Release(UInt32 inFrame)
+{
+	SynthNote::Release(inFrame);
+#if DEBUG_PRINT
+	printf("TestNote::Release %08X %d\n", this, GetState());
+#endif
+}
+
+void			TestNote::FastRelease(UInt32 inFrame) // voice is being stolen.
+{
+	SynthNote::Release(inFrame);
+#if DEBUG_PRINT
+	printf("TestNote::Release %08X %d\n", this, GetState());
+#endif
+}
+
+void			TestNote::Kill(UInt32 inFrame) // voice is being stolen.
+{
+	SynthNote::Kill(inFrame);
+#if DEBUG_PRINT
+	printf("TestNote::Kill %08X %d\n", this, GetState());
+#endif
+}
+
+OSStatus		TestNote::Render(UInt32 inNumFrames, AudioBufferList& inBufferList)
+{
+	float *left, *right;
+/* ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~	
+	Changes to this parameter (kGlobalVolumeParam) are not being de-zippered; 
+	Left as an exercise for the reader
+ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ ~ */
+	float globalVol = GetGlobalParameter(kGlobalVolumeParam);
+	
+	int numChans = inBufferList.mNumberBuffers;
+	if (numChans > 2) return -1;
+	
+	left = (float*)inBufferList.mBuffers[0].mData;
+	right = numChans == 2 ? (float*)inBufferList.mBuffers[1].mData : 0;
+
+	double sampleRate = SampleRate();
+	double freq = Frequency() * (twopi/sampleRate);
+
+	
+#if DEBUG_PRINT_RENDER
+	printf("TestNote::Render %08X %d %g %g\n", this, GetState(), phase, amp);
+#endif
+	switch (GetState())
+	{
+		case kNoteState_Attacked :
+		case kNoteState_Sostenutoed :
+		case kNoteState_ReleasedButSostenutoed :
+		case kNoteState_ReleasedButSustained :
+			{
+				for (UInt32 frame=0; frame<inNumFrames; ++frame)
+				{
+					if (amp < maxamp) amp += up_slope;
+					float out = pow5(sin(phase)) * amp * globalVol;
+					phase += freq;
+					if (phase > twopi) phase -= twopi;
+					left[frame] += out;
+					if (right) right[frame] += out;
+				}
+			}
+			break;
+			
+		case kNoteState_Released :
+			{
+				UInt32 endFrame = 0xFFFFFFFF;
+				for (UInt32 frame=0; frame<inNumFrames; ++frame)
+				{
+					if (amp > 0.0) amp += dn_slope;
+					else if (endFrame == 0xFFFFFFFF) endFrame = frame;
+					float out = pow5(sin(phase)) * amp * globalVol;
+					phase += freq;
+					left[frame] += out;
+					if (right) right[frame] += out;
+				}
+				if (endFrame != 0xFFFFFFFF) {
+#if DEBUG_PRINT
+					printf("TestNote::NoteEnded  %08X %d %g %g\n", this, GetState(), phase, amp);
+#endif
+					NoteEnded(endFrame);
+				}
+			}
+			break;
+			
+		case kNoteState_FastReleased :
+			{
+				UInt32 endFrame = 0xFFFFFFFF;
+				for (UInt32 frame=0; frame<inNumFrames; ++frame)
+				{
+					if (amp > 0.0) amp += fast_dn_slope;
+					else if (endFrame == 0xFFFFFFFF) endFrame = frame;
+					float out = pow5(sin(phase)) * amp * globalVol;
+					phase += freq;
+					left[frame] += out;
+					if (right) right[frame] += out;
+				}
+				if (endFrame != 0xFFFFFFFF) {
+#if DEBUG_PRINT
+					printf("TestNote::NoteEnded  %08X %d %g %g\n", this, GetState(), phase, amp);
+#endif
+					NoteEnded(endFrame);
+				}
+			}
+			break;
+		default :
+			break;
+	}
+	return noErr;
+}
+
---
+++ SinSynthWithMidi.cpp
@@ -0,0 +1,276 @@
+/*	Copyright © 2007 Apple Inc. All Rights Reserved.
+	
+	Disclaimer: IMPORTANT:  This Apple software is supplied to you by 
+			Apple Inc. ("Apple") in consideration of your agreement to the
+			following terms, and your use, installation, modification or
+			redistribution of this Apple software constitutes acceptance of these
+			terms.  If you do not agree with these terms, please do not use,
+			install, modify or redistribute this Apple software.
+			
+			In consideration of your agreement to abide by the following terms, and
+			subject to these terms, Apple grants you a personal, non-exclusive
+			license, under Apple's copyrights in this original Apple software (the
+			"Apple Software"), to use, reproduce, modify and redistribute the Apple
+			Software, with or without modifications, in source and/or binary forms;
+			provided that if you redistribute the Apple Software in its entirety and
+			without modifications, you must retain this notice and the following
+			text and disclaimers in all such redistributions of the Apple Software. 
+			Neither the name, trademarks, service marks or logos of Apple Inc. 
+			may be used to endorse or promote products derived from the Apple
+			Software without specific prior written permission from Apple.  Except
+			as expressly stated in this notice, no other rights or licenses, express
+			or implied, are granted by Apple herein, including but not limited to
+			any patent rights that may be infringed by your derivative works or by
+			other works in which the Apple Software may be incorporated.
+			
+			The Apple Software is provided by Apple on an "AS IS" basis.  APPLE
+			MAKES NO WARRANTIES, EXPRESS OR IMPLIED, INCLUDING WITHOUT LIMITATION
+			THE IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY AND FITNESS
+			FOR A PARTICULAR PURPOSE, REGARDING THE APPLE SOFTWARE OR ITS USE AND
+			OPERATION ALONE OR IN COMBINATION WITH YOUR PRODUCTS.
+			
+			IN NO EVENT SHALL APPLE BE LIABLE FOR ANY SPECIAL, INDIRECT, INCIDENTAL
+			OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+			SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+			INTERRUPTION) ARISING IN ANY WAY OUT OF THE USE, REPRODUCTION,
+			MODIFICATION AND/OR DISTRIBUTION OF THE APPLE SOFTWARE, HOWEVER CAUSED
+			AND WHETHER UNDER THEORY OF CONTRACT, TORT (INCLUDING NEGLIGENCE),
+			STRICT LIABILITY OR OTHERWISE, EVEN IF APPLE HAS BEEN ADVISED OF THE
+			POSSIBILITY OF SUCH DAMAGE.
+*/
+/*
+	This is a subclass of SinSynth that demonstrates how to use the midi output properties kAudioUnitProperty_MIDIOutputCallbackInfo, and kAudioUnitProperty_MIDIOutputCallback
+	defined in AudioUnitProperties.h. Using these properties, the SinSynthWithMidi simply passes through the midi data it receives. Use of these properties requires host support.
+	
+	To build a version of the SinSynth with this functionality, activate the "SinSynth with MIDI Output" target in Xcode.
+*/
+
+#include "SinSynth.h"
+#include <vector>
+
+typedef struct MIDIMessageInfoStruct {
+	UInt8	status;
+	UInt8	channel;
+	UInt8	data1;
+	UInt8	data2;
+	UInt32	startFrame;
+} MIDIMessageInfoStruct;
+
+
+class MIDIOutputCallbackHelper {
+public:
+							MIDIOutputCallbackHelper() {mMIDIMessageList.reserve (16); mMIDICallbackStruct.midiOutputCallback = NULL;}
+	virtual					~MIDIOutputCallbackHelper() {};
+	
+	void					SetCallbackInfo(AUMIDIOutputCallback callback, void *userData) {mMIDICallbackStruct.midiOutputCallback = callback; mMIDICallbackStruct.userData = userData;}
+	
+	void					AddEvent(	UInt8	status,
+										UInt8	channel,
+										UInt8	data1,
+										UInt8	data2, 
+										UInt32	inStartFrame );
+							
+	void					FireAtTimeStamp(const AudioTimeStamp &inTimeStamp);
+
+protected:
+	typedef std::vector<MIDIMessageInfoStruct> MIDIMessageList;
+	
+private:
+	MIDIPacketList		  * PacketList() {return (MIDIPacketList *)mBuffersAllocated; }
+	Byte					mBuffersAllocated[1024];
+	
+	AUMIDIOutputCallbackStruct	mMIDICallbackStruct;
+
+	MIDIMessageList			mMIDIMessageList;
+};
+
+class SinSynthWithMidi : public SinSynth {
+public:
+								SinSynthWithMidi(ComponentInstance inComponentInstance);
+	virtual						~SinSynthWithMidi();
+	
+	virtual OSStatus			GetPropertyInfo(		AudioUnitPropertyID				inID,
+														AudioUnitScope					inScope,
+														AudioUnitElement				inElement,
+														UInt32 &						outDataSize,
+														Boolean &						outWritable);
+	
+	virtual OSStatus			GetProperty(			AudioUnitPropertyID				inID,
+														AudioUnitScope					inScope,
+														AudioUnitElement				inElement,
+														void *							outData);
+														
+	virtual OSStatus			SetProperty(			AudioUnitPropertyID 			inID,
+														AudioUnitScope 					inScope,
+														AudioUnitElement 				inElement,
+														const void *					inData,
+														UInt32 							inDataSize);
+														
+			OSStatus			HandleMidiEvent(		UInt8							status, 
+														UInt8							channel, 
+														UInt8							data1, 
+														UInt8							data2, 
+														UInt32							inStartFrame);
+	
+			OSStatus			Render(					AudioUnitRenderActionFlags &	ioActionFlags,
+														const AudioTimeStamp &			inTimeStamp,
+														UInt32							inNumberFrames);
+
+private:
+	MIDIOutputCallbackHelper mCallbackHelper;
+	
+	TestNote mTestNotes[kNumNotes];
+};
+
+#pragma mark MIDIOutputCallbackHelper Methods
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	MIDIOutputCallbackHelper::AddEvent
+//
+// 
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+void MIDIOutputCallbackHelper::AddEvent(UInt8 status, UInt8 channel,
+										UInt8 data1, UInt8 data2, 
+										UInt32 inStartFrame) {
+	MIDIMessageInfoStruct info = {status, channel, data1, data2, inStartFrame};	
+	mMIDIMessageList.push_back(info);
+}
+
+void MIDIOutputCallbackHelper::FireAtTimeStamp(const AudioTimeStamp &inTimeStamp) {
+	if (!mMIDIMessageList.empty() && mMIDICallbackStruct.midiOutputCallback != 0) {
+		// synthesize the packet list and call the MIDIOutputCallback
+		// iterate through the vector and get each item
+				
+		std::vector<MIDIMessageInfoStruct>::iterator myIterator;
+		MIDIPacketList *pktlist = PacketList();
+
+		for (myIterator = mMIDIMessageList.begin(); myIterator != mMIDIMessageList.end(); myIterator++) {
+			MIDIMessageInfoStruct item = *myIterator;
+			
+			MIDIPacket *pkt = MIDIPacketListInit(pktlist);
+			bool tooBig = false;
+			
+			Byte data[4] = {item.status, item.channel, item.data1, item.data2};
+			if ((pkt = MIDIPacketListAdd(pktlist, sizeof(mBuffersAllocated), pkt, item.startFrame, 4, const_cast<Byte*>(data))) == NULL)
+				tooBig = true;
+				
+			if (tooBig) {	// send what we have and then clear the buffer and send again
+				// issue the callback with what we got
+				OSStatus result = mMIDICallbackStruct.midiOutputCallback(mMIDICallbackStruct.userData, &inTimeStamp, 0, pktlist);
+				if (result != noErr)
+					printf("error calling output callback: %d", (int) result);
+				
+				// clear stuff we've already processed, and fire again
+				mMIDIMessageList.erase(mMIDIMessageList.begin(), myIterator);
+				this->FireAtTimeStamp(inTimeStamp);
+				return;
+			}
+		}
+		
+		// fire callback
+		OSStatus result = mMIDICallbackStruct.midiOutputCallback(mMIDICallbackStruct.userData, &inTimeStamp, 0, pktlist);
+		if (result != noErr)
+			printf("error calling output callback: %d", (int) result);
+
+		mMIDIMessageList.clear();
+	}
+}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+#pragma mark SinSynthWithMidi Methods
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+
+COMPONENT_ENTRY(SinSynthWithMidi)
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	SinSynthWithMidi::SinSynthWithMidi
+//
+// This synth has No inputs, One output
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SinSynthWithMidi::SinSynthWithMidi(ComponentInstance inComponentInstance)
+	: SinSynth(inComponentInstance)
+{}
+
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+//	SinSynthWithMidi::~SinSynthWithMidi
+//
+//~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+SinSynthWithMidi::~SinSynthWithMidi()
+{}
+
+OSStatus			SinSynthWithMidi::GetPropertyInfo(		AudioUnitPropertyID				inID,
+													AudioUnitScope					inScope,
+													AudioUnitElement				inElement,
+													UInt32 &						outDataSize,
+													Boolean &						outWritable)
+{
+	if (inScope == kAudioUnitScope_Global) {
+		if (inID == kAudioUnitProperty_MIDIOutputCallbackInfo) {
+			outDataSize = sizeof(CFArrayRef);
+			outWritable = false;
+			return noErr;
+		} else if (inID == kAudioUnitProperty_MIDIOutputCallback) {
+			outDataSize = sizeof(AUMIDIOutputCallbackStruct);
+			outWritable = true;
+			return noErr;
+		}
+	}
+	return MusicDeviceBase::GetPropertyInfo (inID, inScope, inElement, outDataSize, outWritable);
+}
+
+OSStatus			SinSynthWithMidi::GetProperty(	AudioUnitPropertyID		inID,
+											AudioUnitScope			inScope,
+											AudioUnitElement		inElement,
+											void *					outData)
+{
+	if (inScope == kAudioUnitScope_Global) {
+		if (inID == kAudioUnitProperty_MIDIOutputCallbackInfo) {
+			CFStringRef strs[1];
+			strs[0] = CFSTR("Default Stream");
+			
+			CFArrayRef callbackArray = CFArrayCreate(NULL, (const void **)strs, 1, &kCFTypeArrayCallBacks);
+			outData = (void *) callbackArray;
+			return noErr;
+		}
+	}
+	return AUBase::GetProperty (inID, inScope, inElement, outData);
+}
+
+OSStatus			SinSynthWithMidi::SetProperty(	AudioUnitPropertyID 			inID,
+													AudioUnitScope 					inScope,
+													AudioUnitElement 				inElement,
+													const void *					inData,
+													UInt32 							inDataSize)
+{
+	if (inScope == kAudioUnitScope_Global) {
+		if (inID == kAudioUnitProperty_MIDIOutputCallback) {
+			if (inDataSize < sizeof(AUMIDIOutputCallbackStruct)) return kAudioUnitErr_InvalidPropertyValue;
+			
+			AUMIDIOutputCallbackStruct *callbackStruct = (AUMIDIOutputCallbackStruct *)inData;
+			mCallbackHelper.SetCallbackInfo(callbackStruct->midiOutputCallback, callbackStruct->userData);
+			return noErr;
+		}
+	}
+	return kAudioUnitErr_InvalidProperty;
+}
+
+OSStatus 	SinSynthWithMidi::HandleMidiEvent(UInt8 status, UInt8 channel, UInt8 data1, UInt8 data2, UInt32 inStartFrame) {
+	// snag the midi event and then store it in a vector
+	
+	mCallbackHelper.AddEvent(status, channel, data1, data2, inStartFrame);
+	
+	return AUMIDIBase::HandleMidiEvent(status, channel, data1, data2, inStartFrame);
+}
+
+OSStatus	SinSynthWithMidi::Render(   AudioUnitRenderActionFlags &		ioActionFlags,
+												const AudioTimeStamp &		inTimeStamp,
+												UInt32						inNumberFrames) {
+	OSStatus result = AUInstrumentBase::Render(ioActionFlags, inTimeStamp, inNumberFrames);
+	if (result == noErr) {
+		mCallbackHelper.FireAtTimeStamp(inTimeStamp);
+	} 
+	return result;
+}
